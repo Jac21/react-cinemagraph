d981fe0a7e543c24e0d575ec8767c622fecca351
Jeremy Cantu <jcantu521@gmail.com>
2018-05-15 11:22:34 -0500

    Some styling and props added

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,13 +1,28 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
 
-import Example from '../../src'
+import Cinemagraph from '../../src'
 
 class Demo extends Component {
+  state = {
+    loading: false,
+    fallbackImage: "./demo/assets/Disco.jpg",
+    fallbackImageAlt: "Disco",
+    mp4Source: "./demo/assets/Disco.mp4"
+  }
+
+  handleToggleLoading = () => {
+    this.setState({loading: !this.state.loading})
+  }
+
   render() {
     return <div>
       <h1>react-cinemagraph Demo</h1>
-      <Example/>
+      <Cinemagraph loading={this.state.loading} 
+      fallbackImage={this.state.fallbackImage} 
+      fallbackImageAlt ={this.state.fallbackImageAlt} 
+      mp4Source={this.state.mp4Source}>
+      </Cinemagraph>
     </div>
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,35 @@
-import React, { Component } from "react";
+import t from 'prop-types'
+import React, {Component} from 'react'
 
-export default class extends Component {
+import './css/styles.css'
+
+class Cinemagraph extends Component {
+  static propTypes = {
+    disabled: t.bool,
+    loading: t.bool,
+    fallbackImage: t.string,
+    fallbackImageAlt: t.string,
+    mp4Source: t.string
+  }
+  static defaultProps = {
+    disabled: false,
+    loading: false
+  }
   render() {
-    return (
-      <div>
-        <h2>Welcome to React components</h2>
-      </div>
-    );
+    return(
+    <div className="homepage-hero-module">
+        <div className="video-container">
+            <div className="poster hidden">
+                <img src={this.props.fallbackImage} alt={this.props.fallbackImageAlt} />
+            </div>
+            <div className="filter"></div>
+            <video autoPlay muted loop className="fillWidth">
+                <source src={this.props.mp4Source} type="video/mp4" />Your browser does not support the video tag. I suggest you upgrade your browser.
+            </video>
+        </div>
+    </div>
+    )
   }
 }
+
+export default Cinemagraph
