ecf5c099c4cf2854474fbfb53639a9d1b380e5b9
Jeremy Cantu <jcantu521@gmail.com>
2018-05-29 17:48:55 -0500

    Abstraction

---
+++ src/Cinemagraph.js
@@ -0,0 +1,102 @@
+import t from 'prop-types';
+import React, { Component } from 'react';
+
+import './css/styles.css';
+
+class Cinemagraph extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    this.scaleVideoContainer();
+
+    this.initBannerVideoSize('.video-container .poster img');
+    this.initBannerVideoSize('.video-container .filter');
+    this.initBannerVideoSize('.video-container video');
+
+    window.addEventListener('resize', () => {
+      this.scaleVideoContainer();
+      this.scaleBannerVideoSize('.video-container .poster img');
+      this.scaleBannerVideoSize('.video-container .filter');
+      this.scaleBannerVideoSize('.video-container video');
+    });
+  }
+
+  scaleVideoContainer() {
+    const height = window.innerHeight + 5;
+    const unitHeight = `${parseInt(height, 10)}px`;
+    document.getElementsByClassName('homepage-hero-module')[0].style.height = unitHeight;
+  }
+
+  initBannerVideoSize(elements) {
+    document.querySelectorAll(elements).forEach(element => {
+      element.setAttribute('data-height', `${parseInt(element.height, 10)}px`);
+      element.setAttribute('data-width', `${parseInt(element.width, 10)}px`);
+    });
+
+    this.scaleBannerVideoSize(elements);
+  }
+
+  scaleBannerVideoSize(elements) {
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight + 5;
+    let videoWidth = 0;
+    let videoHeight = 0;
+
+    document.querySelectorAll(elements).forEach(element => {
+      const videoAspectRatio =
+        element.getAttribute('data-height') / element.getAttribute('data-width');
+
+      this.innerWidth = windowWidth;
+
+      if (windowWidth < 1000) {
+        videoHeight = windowHeight;
+        videoWidth = videoHeight / videoAspectRatio;
+        document.querySelectorAll('body')[0].style.marginTop = 0;
+        document.querySelectorAll('body')[0].style.marginLeft = `${-(videoWidth - windowWidth) /
+          2}px`;
+
+        document.querySelectorAll('body')[0].innerWidth = `${videoWidth}px`;
+        document.querySelectorAll('body')[0].innerHeight = `${videoHeight}px`;
+      }
+
+      document
+        .querySelectorAll('.homepage-hero-module .video-container video')[0]
+        .classList.add('fadeIn');
+      document
+        .querySelectorAll('.homepage-hero-module .video-container video')[0]
+        .classList.add('animated');
+    });
+  }
+
+  render() {
+    return (
+      <div className="homepage-hero-module">
+        <div className="video-container">
+          <div className="poster hidden">
+            <img src={this.props.fallbackImage} alt={this.props.fallbackImageAlt} />{' '}
+          </div>{' '}
+          <div className="filter" />{' '}
+          <video autoPlay muted loop className="fillWidth">
+            <source src={this.props.mp4Source} type="video/mp4" /> Your browser does not support the
+            video tag. I suggest you upgrade your browser.{' '}
+            <source src={this.props.webmSource} type="video/webm" />Your browser does not support
+            the video tag. I suggest you upgrade your browser.
+          </video>{' '}
+        </div>{' '}
+      </div>
+    );
+  }
+}
+
+Cinemagraph.propTypes = {
+  fallbackImage: t.string,
+  fallbackImageAlt: t.string,
+  mp4Source: t.string,
+  webmSource: t.string
+};
+
+export { Cinemagraph };
+export default Cinemagraph;
