bf2ed74e7432330ea6ec72f26fdc36fc20125a01
Jeremy Cantu <jcantu521@gmail.com>
2018-05-30 16:50:33 -0500

    Minor ESLint analysis fix

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-import Cinemagraph from '../../src';
+import { Cinemagraph } from '../../src';
 
 class Demo extends Component {
   state = {
