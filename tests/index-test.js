0ede17a340d9a836d55a87d56814f7b0ee11e566
Jeremy Cantu <jcantu521@gmail.com>
2018-06-26 18:26:39 -0500

    Test update

--- tests/index-test.js
+++ tests/index-test.js
@@ -6,8 +6,10 @@ import { Cinemagraph } from '../src/index';
 
 describe('Cinemagraph', () => {
   it('renders a div-wrapped video with default properties', () => {
-    expect(renderToStaticMarkup(<Cinemagraph />)).toContain(
-      '<div class="homepage-hero-module"><div class="video-container"><div class="poster hidden"><img/> </div> <div class="filter"></div> <video autoplay="" muted="" loop="" class="fillWidth"><source type="video/mp4"/> Your browser does not support the video tag. I suggest you upgrade your browser. <source type="video/webm"/>Your browser does not support the video tag. I suggest you upgrade your browser.</video> </div> </div>'
+    expect(
+      renderToStaticMarkup(<Cinemagraph height={50} maxHeight={100} />)
+    ).toContain(
+      '<div class="homepage-hero-module"><div class="video-container" style="height:50vw;max-height:100vh"><div class="poster hidden"><img/> </div> <div class="filter"></div> <video autoplay="" muted="" loop="" class="fillWidth"><source type="video/mp4"/> Your browser does not support the video tag. I suggest you upgrade your browser. <source type="video/webm"/>Your browser does not support the video tag. I suggest you upgrade your browser.</video> </div> </div>'
     );
   });
 });
